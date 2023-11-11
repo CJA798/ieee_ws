dd6d6002aa3d943fa87f2d42b6070c33eade2c60
CJA798 <cjacjacja@hotmail.com>
2023-11-11 16:50:38 -0500

    Nested state machine for small packages added

--- src/state_machine/scripts/state_machine.py
+++ src/state_machine/scripts/state_machine.py
@@ -56,7 +56,7 @@ class PickUpBigPackages(smach.State):
         rospy.loginfo('Executing state PickUpBigPackages')
         # Run LED reading logic
         self.done = True
-        rospy.sleep(5)
+        rospy.sleep(15)
 
         if self.done:
             return 'succeeded'
@@ -78,6 +78,40 @@ class PickUpSmallPackages(smach.State):
         if self.done:
             return 'succeeded'
         
+        return 'aborted'
+
+class SetPose(smach.State):
+    def __init__(self, pose):
+        self.pose = pose
+        smach.State.__init__(self, outcomes=['succeeded','aborted'])
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing state SetPose(scan)')
+        rospy.sleep(5)
+        if self.pose:
+            return 'succeeded'
+        return 'aborted'
+
+class GetCoords(smach.State):
+    def __init__(self, object_type, pose):
+        smach.State.__init__(self, outcomes=['succeeded','aborted'])
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing state GetCoords(small_packages, scan)')
+        rospy.sleep(1)
+        if True:
+            return 'succeeded'
+        return 'aborted'
+
+class PickUp(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['succeeded','aborted'])
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing state PickUp')
+        rospy.sleep(2)
+        if True:
+            return 'succeeded'
         return 'aborted'
 
 # define state GoToDropOff
@@ -104,21 +138,36 @@ def main():
         smach.StateMachine.add('INITIALIZE', Initialize(), 
                                transitions={'succeeded':'READING_START_LED', 'aborted':'INITIALIZE'})
         smach.StateMachine.add('READING_START_LED', ReadingStartLED(), 
-                               transitions={'succeeded':'SM_BOXES', 'aborted':'READING_START_LED'})
+                               transitions={'succeeded':'PACKAGE_PICKUP', 'aborted':'READING_START_LED'})
 
         # Create the sub SMACH state machine
-        sm_boxes = smach.Concurrence(outcomes=['succeeded','aborted'],
+        sm_packages = smach.Concurrence(outcomes=['succeeded','aborted'],
                                     default_outcome='aborted',
                                     outcome_map={'succeeded':{
                                         'PICK_BIG_PACKAGES':'succeeded',
                                         'PICK_SMALL_PACKAGES':'succeeded'
                                     }})
 
-        with sm_boxes:
-            smach.Concurrence.add('PICK_BIG_PACKAGES', PickUpBigPackages())
-            smach.Concurrence.add('PICK_SMALL_PACKAGES', PickUpSmallPackages())
+        with sm_packages:
+            sm_small_packages = smach.StateMachine(outcomes=['succeeded'])
 
-        smach.StateMachine.add('SM_BOXES', sm_boxes,
+            with sm_small_packages:
+                smach.StateMachine.add('SCAN_POSE', SetPose('scan'),
+                                        transitions={'succeeded':'GET_SP_COORDS', 'aborted':'SCAN_POSE'})
+                smach.StateMachine.add('GET_SP_COORDS', GetCoords(object_type='small_package', pose='scan'),
+                                        transitions={'succeeded':'VERIFY_POSE', 'aborted':'GET_SP_COORDS'})
+                smach.StateMachine.add('VERIFY_POSE', SetPose('verify'),
+                                        transitions={'succeeded':'VERIFY_COORDS', 'aborted':'VERIFY_POSE'})
+                smach.StateMachine.add('VERIFY_COORDS', GetCoords(object_type='small_package', pose='verify'),
+                                        transitions={'succeeded':'PICK_UP', 'aborted':'VERIFY_COORDS'})
+                smach.StateMachine.add('PICK_UP', PickUp(),
+                                        transitions={'succeeded':'succeeded', 'aborted':'PICK_UP'})
+                                        
+                                        
+            smach.Concurrence.add('PICK_BIG_PACKAGES', PickUpBigPackages())
+            smach.Concurrence.add('PICK_SMALL_PACKAGES', sm_small_packages)
+
+        smach.StateMachine.add('PACKAGE_PICKUP', sm_packages,
                                 transitions={'succeeded':'GO_TO_DROP_OFF',
                                             'aborted':'INITIALIZE'})
         
@@ -126,7 +175,7 @@ def main():
                                 transitions={'succeeded':'END'})
 
     # Create and start the introspection server
-    sis = smach_ros.IntrospectionServer('server_name', sm, '/SM_ROOT')
+    sis = smach_ros.IntrospectionServer('server_name', sm, '/START')
     sis.start()
 
     # Execute SMACH plan
