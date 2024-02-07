7941070aaa7ad4db90a48130193d3951148457e3
maximus919 <mturcios@unca.edu>
2024-02-06 21:03:46 -0500

    beginning the State machine

--- src/navigation_system/src/navigation_SM.cpp
+++ src/navigation_system/src/navigation_SM.cpp
@@ -23,6 +23,17 @@ int tofBack;
 int bearing;
 int gravVector;
 
+//variables for the movement arrays
+int Stop = [0,0,0];
+int Go = [0,0,0];
+int Backwards = [0,0,0];
+int TurnCW = [0,0,0];
+int TurnCCW = [0,0,0];
+int Go_Right = [0,0,0];
+int Go_Left = [0,0,0];
+
+//N
+
 
 // create subscriber callbacks
 void tofOneCallback(const std_msgs::Int16::ConstPtr& msg){
@@ -82,6 +93,169 @@ int main(int argc, char **argv) {
         wheelSpeeds.data = {wheelSpeedOne, wheelSpeedTwo, wheelSpeedThree};
         nav_state_pub.publish(navState);
         wheel_speed_pub.publish(wheelSpeeds);
+
+          /*
+        switch(event){
+            case STOP:
+            //All motors off
+            wheelSpeeds.data = {0,0,0};
+            wheel_speed_pub.publish(wheelSpeeds);
+            Movement(Stop);
+
+            case BIG_BLOCKS:
+            //motors go forward about 10 in
+
+
+            // Start the timer, Still working on this
+            auto start_time = std::chrono::high_resolution_clock::now();
+    
+            // Perform some task (for demonstration, we'll just sleep for a while)
+            Movement(Go_Forward); // Sleep for 3 seconds
+    
+           // Stop the timer
+            auto end_time = std::chrono::high_resolution_clock::now();
+
+            //After a certain amount of time, stop
+            Movement(Stop);
+
+            //block collection will take place, announce state to be "ready for collection"
+
+            case 
+            */
+            
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+        /*
+        //THIS WORKS FOR CLOCKWISE TURNS 90 AND 180 NOT 360
+void TurnCW(int degrees){  //put in the angle you would like the bot to end up facing on the board (in reference to starting position) 270 -> Left, 180 -> backwards, 360/0 -> straight ahead
+
+   //double turning = desired_orientation + degrees; // degrees can be 90, 180
+   //option = 0;
+    double difference = current_orientation - desired_orientation;
+    if(desired_orientation + degrees > 360){
+      degrees = (desired_orientation + degrees) - 360;
+      option = 1;
+    }
+    else {
+      degrees = desired_orientation + degrees;
+      option = 2;
+    }
+
+    switch(option){
+
+      case 1:
+
+    if(current_orientation <= 360 && current_orientation >= 270){
+      Movement(0,0,1);
+      current_orientation = getOrientationX(&orientationData);
+    }
+    else if(current_orientation <= degrees){
+        Movement(0,0,1);
+        current_orientation = getOrientationX(&orientationData);
+      }
+
+  else{
+     Movement(0,0,0);
+     event = PHASE_1;
+  }
+
+
+    break;
+
+
+    case 2:
+
+     if(current_orientation <= degrees){
+        Movement(0,0,1);
+        current_orientation = getOrientationX(&orientationData);
+      }
+
+  else {
+    Movement(0,0,0);
+    event = PHASE_1;
+  }
+
+
+
+
+
+
+    break;
+
+    default:break;
+
+}
+}
+
+void TurnCCW(int degrees){  //put in the angle you would like the bot to end up facing on the board (in reference to starting position) 270 -> Left, 180 -> backwards, 360/0 -> straight ahead
+    double difference = current_orientation - desired_orientation;
+
+  //  double old_curr = current_orientation; 
+    if(desired_orientation - degrees < 0){
+      degrees = 360 + (desired_orientation - degrees);
+      option = 1;
+    }
+    else {
+      degrees = desired_orientation - degrees;
+      option = 2;
+    }
+
+    switch(option){
+      case 1:
+       if(350 >= current_orientation && current_orientation <=90){
+      Movement(0,0,-1);
+      current_orientation = getOrientationX(&orientationData);
+    }
+    else if(current_orientation >= degrees){
+        Movement(0,0,-1);
+        current_orientation = getOrientationX(&orientationData);
+      }
+
+  else{
+   Movement(0,0,0);
+   event = PHASE_1;
+  }
+
+
+      break;
+
+
+      case 2:
+      if(current_orientation >= degrees){
+        Movement(0,0,-1);
+        current_orientation = getOrientationX(&orientationData);
+      }
+
+  else{
+   Movement(0,0,0);
+   event = PHASE_1;
+  }
+
+
+
+      break;
+
+      default:break;
+
+    }
+        
+        
+        */
 
         // Spin and sleep
         ros::spinOnce();
