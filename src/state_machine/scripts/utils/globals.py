49537708ee40f2eca863d6f4473eb743053aab38
CJA798 <cjacjacja@hotmail.com>
2024-02-21 19:08:22 -0500

    SM works up to drop bridge

--- src/state_machine/scripts/utils/globals.py
+++ src/state_machine/scripts/utils/globals.py
@@ -5,7 +5,7 @@ globals: Dict[str, Union[bool, int, None]] = {
     'gravity_vector': 0,
     'bearing': 0,
     'move_done': 0,
-    'arm'
+    # TODO: Add misc_done when implemented
     'tof_front': None,
     'tof_back': None,
     'test_global': 0
