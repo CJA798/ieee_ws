654f5bf8bebe3e5412ae0e5d3aa31703b6b95811
cdbass93 <cdbass93@gmail.com>
2024-02-26 19:30:50 -0500

    debug

--- src/servos/src/servos.cpp
+++ src/servos/src/servos.cpp
@@ -174,7 +174,7 @@ public:
             packetHandler->write2ByteTxOnly(portHandler, ARM_SECONDARY_ID, POS_D_GAIN_ADDR, POS_D_GAIN);            // Sets posistion D gain
 
             // Publish 8 starting servo angles and speed to Arm_Angles
-            int Arm_Start_Angles[9] = { 1586, 2902, 2898, 1471, 2063, 1802, 1041, 1965, 1 };
+            int Arm_Start_Angles[9] = { 1586, 2902, 2898, 1471, 2063, 1802, 1041, 1980, 1 };
             for (int i = 0; i < 9; i++) {
                 Arm_Angles.data[i] = Arm_Start_Angles[i];
             }
